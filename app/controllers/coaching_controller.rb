b95ab070533df5a5a293ee7b3d0d609123ed5365
fahad alkubaisi <alkubaisi9@gmail.com>
2017-02-13 13:31:48 +0000

    done

---
+++ app/controllers/coaching_controller.rb
@@ -0,0 +1,17 @@
+
+
+class CoachingController < ApplicationController
+ def answer
+   @query = params[:query]
+   if @query =~ /I am going to train/i
+     @answer = "Go go go go !!"
+   elsif @query.ends_with?("?") # =~ /.*\?$/
+     @answer = "Silly question, go and train!"
+   else
+     @answer = "I don't care, just train!"
+   end
+ end
+
+ def ask
+ end
+end
